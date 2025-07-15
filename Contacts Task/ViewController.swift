a09c471c86c6cffc0521850667edd16ad8bc8373
Aya Salman <32332151+ayasalman@users.noreply.github.com>
2025-07-15 23:34:08 +0300

    Initial Commit

---
+++ Contacts Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts Task
+//
+//  Created by Aya Salman on 15/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
